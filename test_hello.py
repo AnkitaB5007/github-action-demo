5e676de033f0c988511aa4c09ea079fae202679c
Ankita Behura <ankitabehura@gmail.com>
2025-04-26 20:56:09 +0200

    Github actions setup

---
+++ test_hello.py
@@ -0,0 +1,8 @@
+from hello import say_hello
+
+def test_say_hello():
+    assert say_hello("World") == "Hello, World!"
+    assert say_hello("Alice") == "Hello, Alice!"
+    assert say_hello("Bob") == "Hello, Bob!"
+    assert say_hello("") == "Hello, !"
+    assert say_hello("123") == "Hello, 123!"
